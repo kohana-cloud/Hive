ab9f49119485b0969a55c08823f32a90993ec335
Nate Singer <nathaniel@singer.cloud>
2022-05-08 17:30:21 -0700

    updates

--- src/code/Honeypots.py
+++ src/code/Honeypots.py
@@ -1,24 +1,36 @@
 import yaml
 
 class Honeypot():
-    def __init__(self, id:str, owner:str, health:str, last_update:int=None, type:str=None) -> None:
+    def __init__(self, id:str, owner_email:str, owner_name:str, health:str, last_update:int=None, type:str=None) -> None:
         self.id = id
-        self.owner = owner
+        self.owner_email = owner_email
+        self.owner_name = owner_name
         self.health = health
         self.last_update = last_update
         self.type = type
 
 
 class VPS(Honeypot):
-    def __init__(self, id:str, owner:str, health:str, last_update:str=None,
-                    os:str=None) -> None:
+    def __init__(self, id:str, owner_email:str, owner_name:str, health:str, last_update:str=None, os:str=None) -> None:
         self.os = os
         Honeypot.__init__(self,
                          id = id,
-                         owner = owner,
+                         owner_email = owner_email,
+                         owner_name = owner_name, 
                          health = health,
                          last_update = last_update,
                          type = "VPS")
+
+class Database(Honeypot):
+    def __init__(self, id:str, owner_email:str, owner_name:str, health:str, last_update:str=None, db_engine:str=None) -> None:
+        self.db_engine = db_engine
+        Honeypot.__init__(self,
+                         id = id,
+                         owner_email = owner_email,
+                         owner_name = owner_name, 
+                         health = health,
+                         last_update = last_update,
+                         type = "Database")
 
 
 def ingest_honeypots(config_file:str) -> None:
@@ -30,12 +42,27 @@ def ingest_honeypots(config_file:str) -> None:
 
             for id, config in zip(config_file['servers'],
                                   config_file['servers'].values()):
-                honeypots.append(VPS(id,
-                    owner = config['owner'],
-                    health = config['health'],
-                    last_update = config['last-updated'],
-                    os = config['os']
-                ))
+                honeypots.append(
+                    VPS(id,
+                        owner_email = config['owner-email'],
+                        owner_name = config['owner-name'],
+                        health = config['health'],
+                        last_update = config['last-updated'],
+                        os = config['os']
+                    )
+                )
+            
+            for id, config in zip(config_file['databases'],
+                                  config_file['databases'].values()):
+                honeypots.append(
+                    Database(id,
+                        owner_email = config['owner-email'],
+                        owner_name = config['owner-name'],
+                        health = config['health'],
+                        last_update = config['last-updated'],
+                        db_engine = config['db-engine']
+                    )
+                )
 
         except yaml.YAMLError as e:
             print(f"Error {e}")
