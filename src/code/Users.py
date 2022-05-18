45dec39f890d88a68f2a6a0373cb31575833c5a0
Nathaniel Singer <nathaniel@singer.cloud>
2022-05-18 01:41:32 -0700

    more auth updates

--- src/code/Users.py
+++ src/code/Users.py
@@ -1,9 +1,6 @@
-import yaml
 from os.path import exists
-import datetime
-import jwt
-import bcrypt
-import time
+import yaml, bcrypt
+
 
 class User():
     def __init__(self, id:int, admin:bool, username:str, email:str, pwhash_salted:str, first:str, last:str, phone:str) -> None:
@@ -19,59 +16,9 @@ class User():
     def check_password(self, password:str) -> None:
         return bcrypt.checkpw(password.encode('ascii'), self.pwhash_salted.encode('ascii'))
 
-
-
-
-
-
-    def __assign_jwt(self): 
-        self.jwt = jwt.encode({
-            'id'    : self.id,
-            'name'  : f"{self.first} {self.last}",
-            'iat'   : datetime.datetime.utcnow(),
-            'exp'   : datetime.datetime.utcnow() 
-                        + datetime.timedelta(minutes=self.app.config['LOGINEXP_MINS'])
-            }, self.app.config['SECRET_KEY'], "HS256")
-    
-    def __update_jwt_exp(self):
-        decoded_jwt_timestamp_notvalidated = jwt.decode(
-            self.jwt, options={"verify_signature": False})['iat']
-
-        self.jwt = jwt.encode({
-            'id'    : self.id,
-            'name'  : f"{self.first} {self.last}",
-            'iat'   : decoded_jwt_timestamp_notvalidated,
-            'exp'   : datetime.datetime.utcnow() 
-                        + datetime.timedelta(minutes=self.app.config['LOGINEXP_MINS'])
-            }, self.app.config['SECRET_KEY'], "HS256")
-
-    def get_jwt(self):
-        # Assign JWT if unassigned
-        if (self.jwt == None): self.__assign_jwt()
-
-        # Remove JWT if expired
-        decoded_jwt_notvalidated = jwt.decode(self.jwt, options={"verify_signature": False})
-        if ((decoded_jwt_notvalidated['exp'] - int(time.time())) < 0):
-            self.jwt = None
-
-        # Assign JWT if unassigned
-        if (self.jwt == None): self.__assign_jwt()
-
-        # Update expiration timestamp
-        self.__update_jwt_exp()
-
-        # Return JWT to caller
-        return self.jwt
-
-    def expire_jwt(self): self.jwt = None
-
-    
-
     def generate_pwhash(self, password:str) -> None:
-        return bcrypt.hashpw(
-            password.encode('ascii'), 
-            bcrypt.gensalt(self.app.config['BCRYPT_ROUNDS'])
-        )
+        return bcrypt.hashpw(password.encode('ascii'), 
+            bcrypt.gensalt(self.app.config['BCRYPT_ROUNDS']))
 
 
 def ingest_users(users_file:str, app:object) -> None:
@@ -100,5 +47,5 @@ def ingest_users(users_file:str, app:object) -> None:
 
         except yaml.YAMLError as e:
             print(f"Error {e}")
-
+            
     return users
